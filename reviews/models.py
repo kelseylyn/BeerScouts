62fa9b484ff4780db69ac5348353d1b42c68d587
kelseylyn <kelseylynzimmer@gmail.com>
2020-11-09 17:32:40 -0800

    Initial commit

---
+++ reviews/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.db.models import Avg
+import numpy as np
+
+
+class Beer(models.Model):
+    name = models.CharField(max_length=200)
+
+    #def average_rating(self):
+        #all_ratings = map(lambda x: x.rating, self.review_set.all())
+        #return np.mean(all_ratings)
+
+    def average_rating(self):
+        return self.review_set.aggregate(Avg('rating'))['rating__avg']
+
+    def __unicode__(self):
+        return self.name
+
+class Review(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
+    beer = models.ForeignKey(Beer, on_delete=models.CASCADE,)
+    pub_date = models.DateTimeField('date published')
+    user_name = models.CharField(max_length=100)
+    comment = models.CharField(max_length=200)
+    rating = models.IntegerField(choices=RATING_CHOICES)
+
+#class Cluster(models.Model):
+    #name = models.CharField(max_length=100)
+    #users = models.ManyToManyField(User)
+
+    #def get_members(self):
+        #return "\n".join([u.username for u in self.users.all()])
+
+# Create your models here.
---
+++ reviews/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path, re_path
+from  . import views
+
+urlpatterns = [
+    # ex: /
+
+    re_path(r'^$', views.review_list, name='review_list'),
+    # ex: /review/5/
+    re_path(r'review/(?P<review_id>[0-9]+)/$', views.review_detail, name='review_detail'),
+    # ex: /beer/
+    re_path(r'^beer$', views.beer_list, name='beer_list'),
+    # ex: /beer/5/
+    re_path(r'^beer/(?P<beer_id>[0-9]+)/$', views.beer_detail, name='beer_detail'),
+
+    re_path(r'^beer/(?P<beer_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
+
+    # ex: /review/user - get reviews for the logged user
+    re_path(r'^review/user/(?P<username>\w+)/$', views.user_review_list, name='user_review_list'),
+    re_path(r'^review/user/$', views.user_review_list, name='user_review_list'),
+
+
+]
+app_name = 'reviews'
